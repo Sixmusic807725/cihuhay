8814c6224817d92d73f70985e162286e03106a9d
Cx12 <56474039+Sixmusic807725@users.noreply.github.com>
2019-10-16 05:55:28 +0700

    Initial commit

---
+++ Cihuhay1.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+#Process Tools
+clear
+
+echo -e "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞" | lolcat
+echo -e "«TUNGGU DAN LIAT LIST TOOLS YG ADA.........»" | lolcat
+echo -e "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞" | lolcat
+
+sleep 2
+echo "\033[0;32m
+______________________
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+ ╔════════════╗
+    Mr.Six
+ ╚════════════╝
+  ║██████████╚╗
+  ║██╔══╗█╔═╗█║
+  ║██║╬╔╝█╚╗║█║
+  ║██╚═╝█║█╚╝█║
+  ╚╗█████████═╝
+     ╚╗║╠╩╩╩╩╩╝
+       ║║┈┈┈█▐█████▒.｡oO
+       ║██╠╦╦╦╗
+       ╚╗██████";
+
+sleep 2
+
+echo $yellow "+<><><><>(Mr.Cx12)<><><><>+"
+echo $green "|+JENIS TOOLS:Shell                               |"
+echo $green  "|+CODING:MR.Six                                    |"
+echo $green "|+CONTAC:081227443846.                    |"
+echo $green "|+THANKS TO:[•]RevoersTeam[•]        |"
+echo $green "|+Instagram:maxusgaming                    |"
+echo $green "|+NO RECODE GW PANTAU!                 |"
+echo $purple "|~Special Thanks to Allah SWT           |"
+echo $green "       +<><>[INDONESIA]<><>+"
+echo $blue "»»========[MENU]=========««"
+echo ""
+echo ""
+
+sleep 1
+
+echo $cy"BERIKUT TOOLS YG SAYA PUNYA"
+
+sleep 1
+
+echo $g"≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠" | lolcat
+echo   "1.HACK FB ALL METODE" | lolcat
+echo $g"≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g"≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠" | lolcat
+echo   "2.BUAT SCRIPT DEFSCE SENDIRI" | lolcat
+echo $g"≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g"≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠" | lolcat
+echo   "3.PENANGKAL VIRUS" | lolcat
+echo $g"≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g"≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠" | lolcat
+echo   "00.KELUAR/EXIT" | lolcat
+echo $g"≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠" | lolcat
+
+sleep 0.1
+
+
+echo $r"╭─[Select Number Of Tools:]"
+read -p "╰─root@SixTampans~# " pil
+
+if [ $pil = "1" ] || [ $pil = "01" ]
+then
+
+figlet -f slant HackFb | lolcat
+
+sleep 1
+
+git clone https://github.com/Sixmusic807725/HackFbBanyakk
+
+cd HackFbBanyakk
+
+sh Banyak.sh
+
+fi
+
+if [ $pil = "2" ] || [ $pil = "02" ]
+then
+
+figlet -f slant Deface | lolcat
+
+sleep 1
+
+git clone https://github.com/MrTamfanX/MrCreateDeface
+
+cd MrCreateDeface
+
+python2 MRCreateDeface.py
+
+fi
+
+if [ $pil = "3" ] || [ $pil = "03" ]
+then
+
+figlet -f slant Penangkal | lolcat
+
+sleep 1
+
+git clone https://github.com/JustAHackers/penangkal
+
+cd penangkal
+
+bash setup
+
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
